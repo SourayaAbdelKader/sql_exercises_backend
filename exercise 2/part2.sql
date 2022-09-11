cfdc4c2ea585bc46729bd27756982076768a87f4
Sourayaak <sourayaabdelkader.gmail.com>
2022-09-11 19:24:13 +0300

    ex2: part3

--- exercise 2/part2.sql
+++ exercise 2/part2.sql
@@ -1,4 +1,4 @@
 /* Find the course or courses with the earliest start time */
 
-select min(start_time) 
+select min(start_time) as earliest_start
 from courses;
---
+++ exercise 2/part3.sql
@@ -0,0 +1,4 @@
+/* Find the number of majors in each department*/ 
+
+select count(distinct(name)) 
+from departments;
